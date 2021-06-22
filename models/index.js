f78ad1b7983bf9451f96e7300c6f33bdfc2c165e
kyle004 <kyleberman92@gmail.com>
2021-06-22 14:20:12 -0700

    added seeds

--- models/index.js
+++ models/index.js
@@ -1,35 +1,35 @@
-const User = require('./User')
-const Bets = require('./Bets')
+const User = require('./User.js')
+const Bets = require('./Bets.js')
 const Witness = require('./Witness')
 const Participant = require('./Participant')
 
 // your relationships go here...
 // creatorid foreign key
 Bets.belongsTo(User, {
-  foreignKey: 'creator_id',
+  foreignKey: 'creator_id'
 })
 User.hasMany(Bets, {
-  foreignKey: 'creator_id',
+  foreignKey: 'creator_id'
 })
 
 // witness foreign key
 Bets.belongsToMany(User, {
   through: Witness,
-  foreignKey: 'bets_id',
+  foreignKey: 'bets_id'
 })
 User.belongsToMany(Bets, {
   through: Witness,
-  foreignKey: 'user_id',
+  foreignKey: 'user_id'
 })
 
 // participant foreign key
 Bets.belongsToMany(User, {
   through: Participant,
-  foreignKey: 'bets_id',
+  foreignKey: 'bets_id'
 })
 User.belongsToMany(Bets, {
   through: Participant,
-  foreignKey: 'user_id',
+  foreignKey: 'user_id'
 })
 
-module.exports = { User, Bets, Participant, Witness}
+module.exports = { User, Bets, Participant, Witness }

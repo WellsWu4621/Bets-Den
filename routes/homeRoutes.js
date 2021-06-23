4ba4210a23cbe84c8212bf3b06a6e87793b9fb4f
Wells Wu <wells.wu4621@gmail.com>
2021-06-23 10:43:42 -0700

    changes

---
+++ routes/homeRoutes.js
@@ -0,0 +1,5 @@
+const router = require('express').Router()
+const { User, Bets, Witness, Participant } = require('../models')
+const passport = require('passport')
+const jwt = require('jsonwebtoken')
+const sequelize = require('../db')

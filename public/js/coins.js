2bf6080b85746942d0c2812c0e609011b646946e
Wells Wu <wells.wu4621@gmail.com>
2021-06-26 19:52:33 -0700

    += profile, + css functionality

--- public/js/coins.js
+++ public/js/coins.js
@@ -5,39 +5,39 @@
  * Modified.
  */
 
-(function($){
+(function ($) {
 
-    var slotMachine = function(){
+    var slotMachine = function () {
 
         var credits = 15,
             spinning = 3,
-            spin = [0,0,0],
+            spin = [0, 0, 0],
             slotsTypes = {
-                'cherry': [2,5,10],
-                'orange': [0,15,30],
-                'prune': [0,40,50],
-                'bell': [0,50,80],
-                'bar1': [0,0,100],
-                'bar2': [0,0,150],
-                'bar3': [0,0,250],
-                'seven': [0,0,500],
-                'anybar': [0,0,80]
+                'cherry': [2, 5, 10],
+                'orange': [0, 15, 30],
+                'prune': [0, 40, 50],
+                'bell': [0, 50, 80],
+                'bar1': [0, 0, 100],
+                'bar2': [0, 0, 150],
+                'bar3': [0, 0, 250],
+                'seven': [0, 0, 500],
+                'anybar': [0, 0, 80]
             },
             slots = [
-                ['orange','bell','orange','bar2','prune','orange',
-                    'bar3','prune','orange','bar1','bell','cherry','orange',
-                    'prune','bell','bar1','cherry','seven','orange','prune',
-                    'orange','bell','orange'],
-                ['cherry','prune','orange','bell','bar1','cherry','prune',
-                    'bar3','cherry','bell','orange','bar1','seven','cherry',
-                    'bar2','cherry','bell','prune','cherry','orange','cherry',
-                    'prune','orange'],
-                ['cherry','orange','bell','prune','bar2','cherry','prune',
-                    'orange','bar3','cherry','bell','orange','cherry','orange',
-                    'cherry','prune','bar1','seven','bell','cherry','cherry',
-                    'orange','bell'],
+                ['orange', 'bell', 'orange', 'bar2', 'prune', 'orange',
+                    'bar3', 'prune', 'orange', 'bar1', 'bell', 'cherry', 'orange',
+                    'prune', 'bell', 'bar1', 'cherry', 'seven', 'orange', 'prune',
+                    'orange', 'bell', 'orange'],
+                ['cherry', 'prune', 'orange', 'bell', 'bar1', 'cherry', 'prune',
+                    'bar3', 'cherry', 'bell', 'orange', 'bar1', 'seven', 'cherry',
+                    'bar2', 'cherry', 'bell', 'prune', 'cherry', 'orange', 'cherry',
+                    'prune', 'orange'],
+                ['cherry', 'orange', 'bell', 'prune', 'bar2', 'cherry', 'prune',
+                    'orange', 'bar3', 'cherry', 'bell', 'orange', 'cherry', 'orange',
+                    'cherry', 'prune', 'bar1', 'seven', 'bell', 'cherry', 'cherry',
+                    'orange', 'bell'],
             ],
-            startSlot = function(){
+            startSlot = function () {
 
                 spinning = false;
 
@@ -48,7 +48,7 @@
                 return false;
 
             },
-            endSlot = function(){
+            endSlot = function () {
 
                 $('#slot-block').show();
                 $('#slot-credits').text('VERLOREN!!!');
@@ -56,34 +56,38 @@
                 setInterval(blink($('#slot-credits')), 1000);
 
             },
-            addCredit = function(incrementCredits){
+            addCredit = function (incrementCredits) {
 
                 var currentCredits = credits;
-                    credits += incrementCredits;
-              axios.post('/Users', {
-                currentCredits: {addCredit}
-              })
-                .then((response) => {
-                  console.log(response);
-                }, (error) => {
-                  console.log(error);
-                });
-
+                credits += incrementCredits;
+                const addCoins = () => {
+                    axios.put('/api/users/tokens', { tokens: credits }, {
+                        headers: {
+                            'Authorization': `Bearer ${localStorage.getItem('token')}`
+                        }
+                    })
+                        .then((response) => {
+                            console.log(response);
+                        }, (error) => {
+                            console.log(error);
+                        });
+                }
+                addCoins()
                 blink($('#slot-credits'));
 
                 $('#slot-credits')
                     .css('credit', 0)
                     .animate({
                         credit: incrementCredits
-                    },{
+                    }, {
                         duration: 400 + incrementCredits,
                         easing: 'easeOut',
-                        step: function (now){
+                        step: function (now) {
 
                             $(this).html(parseInt(currentCredits + now, 10));
 
                         },
-                        complete: function(){
+                        complete: function () {
 
                             $(this).html(credits);
                             blink($('#slot-credits'));
@@ -92,11 +96,11 @@
                     });
 
             },
-            spin = function(){
+            spin = function () {
 
                 this.blur();
 
-                if(spinning == false){
+                if (spinning == false) {
 
                     $('#slot-machine .arm').animate({ top: '45px', height: '2%' });
                     $('#slot-machine .arm .knob').animate({ top: '-20px', height: '20px' });
@@ -104,7 +108,7 @@
                     $('#slot-machine .ring1 .shadow, #slot-machine .ring2 .shadow').animate({ top: '50%', opacity: 1 });
 
                     spinning = 3;
-                    credits --;
+                    credits--;
 
                     $('#slot-credits').html(credits);
 
@@ -120,22 +124,22 @@
                     $('#wheel2 img:first').css('top', - (spin[1] * 44 + 16) + 'px');
                     $('#wheel3 img:first').css('top', - (spin[2] * 44 + 16) + 'px');
 
-                    setTimeout(function(){
+                    setTimeout(function () {
                         $('#slot-machine .arm').animate({ top: '-25px', height: '50%', overflow: 'visible' });
                         $('#slot-machine .arm .knob').animate({ top: '-15px', height: '16px' });
                         $('#slot-machine .arm-shadow').animate({ top: '13px' });
                         $('#slot-machine .ring1 .shadow, #slot-machine .ring2 .shadow').animate({ top: '0', opacity: 0 });
                     }, 500);
 
-                    setTimeout(function(){
+                    setTimeout(function () {
                         stopSpin(1);
                     }, 1500 + parseInt(1500 * Math.random()));
 
-                    setTimeout(function(){
+                    setTimeout(function () {
                         stopSpin(2);
                     }, 1500 + parseInt(1500 * Math.random()));
 
-                    setTimeout(function(){
+                    setTimeout(function () {
                         stopSpin(3);
                     }, 1500 + parseInt(1500 * Math.random()));
 
@@ -144,7 +148,7 @@
                 return false;
 
             },
-            stopSpin = function(slot){
+            stopSpin = function (slot) {
 
                 $('#wheel' + slot)
                     .find('img:last')
@@ -153,21 +157,21 @@
                     .find('img:first')
                     .animate({
                         top: - spin[slot - 1] * 44
-                    },{
+                    }, {
                         duration: 500,
                         easing: 'elasticOut',
-                        complete: function() {
+                        complete: function () {
 
-                            spinning --;
+                            spinning--;
 
-                            if(spinning <= 0){
+                            if (spinning <= 0) {
                                 endSpin();
                             }
 
                         }
                     });
             },
-            endSpin = function(){
+            endSpin = function () {
 
                 var slotType = slots[0][spin[0]],
                     matches = 1,
@@ -175,41 +179,41 @@
                     winnedCredits = 0,
                     waitToSpin = 10;
 
-                if(slotType == slots[1][spin[1]]){
+                if (slotType == slots[1][spin[1]]) {
 
-                    matches ++;
+                    matches++;
 
-                    if(slotType == slots[2][spin[2]]){
-                        matches ++;
-                    } else if(barMatch !=0 && /bar/.test(slots[2][spin[2]])){
-                        barMatch ++;
+                    if (slotType == slots[2][spin[2]]) {
+                        matches++;
+                    } else if (barMatch != 0 && /bar/.test(slots[2][spin[2]])) {
+                        barMatch++;
                     }
 
-                } else if(barMatch != 0 && /bar/.test(slots[1][spin[1]])){
+                } else if (barMatch != 0 && /bar/.test(slots[1][spin[1]])) {
 
-                    barMatch ++;
+                    barMatch++;
 
-                    if(/bar/.test(slots[2][spin[2]])){
-                        barMatch ++;
+                    if (/bar/.test(slots[2][spin[2]])) {
+                        barMatch++;
                     }
 
                 }
 
-                if(matches != 3 && barMatch == 3){
+                if (matches != 3 && barMatch == 3) {
                     slotType = 'anybar';
                     matches = 3;
                 }
 
-                var winnedCredits = slotsTypes[slotType][matches-1];
+                var winnedCredits = slotsTypes[slotType][matches - 1];
 
-                if(winnedCredits > 0){
+                if (winnedCredits > 0) {
                     addCredit(winnedCredits);
                     waitToSpin = 410 + winnedCredits;
                 }
 
-                setTimeout(function(){
+                setTimeout(function () {
 
-                    if(credits == 0){
+                    if (credits == 0) {
                         endSlot();
                     } else {
                         $('#slot-trigger').removeClass('slot-triggerDisabled');
@@ -220,17 +224,17 @@
             };
         return {
 
-            init: function(){
+            init: function () {
 
                 startSlot();
 
                 $('#slot-trigger')
-                    .bind('mousedown', function(){
+                    .bind('mousedown', function () {
                         $(this).addClass('slot-triggerDown');
                     })
                     .bind('click', spin);
 
-                $(document).bind('mouseup', function(){
+                $(document).bind('mouseup', function () {
                     $('#slot-trigger').removeClass('slot-triggerDown');
                 });
 
@@ -243,27 +247,27 @@
         };
     }();
 
-    $.extend($.easing,{
-        bounceOut: function (x, t, b, c, d){
-            if((t/=d) < (1/2.75)){
-                return c*(7.5625*t*t) + b;
-            } else if(t < (2/2.75)){
-                return c*(7.5625*(t-=(1.5/2.75))*t + .75) + b;
-            } else if(t < (2.5/2.75)){
-                return c*(7.5625*(t-=(2.25/2.75))*t + .9375) + b;
+    $.extend($.easing, {
+        bounceOut: function (x, t, b, c, d) {
+            if ((t /= d) < (1 / 2.75)) {
+                return c * (7.5625 * t * t) + b;
+            } else if (t < (2 / 2.75)) {
+                return c * (7.5625 * (t -= (1.5 / 2.75)) * t + .75) + b;
+            } else if (t < (2.5 / 2.75)) {
+                return c * (7.5625 * (t -= (2.25 / 2.75)) * t + .9375) + b;
             } else {
-                return c*(7.5625*(t-=(2.625/2.75))*t + .984375) + b;
+                return c * (7.5625 * (t -= (2.625 / 2.75)) * t + .984375) + b;
             }
         },
-        easeOut: function (x, t, b, c, d){
-            return -c *(t/=d)*(t-2) + b;
+        easeOut: function (x, t, b, c, d) {
+            return -c * (t /= d) * (t - 2) + b;
         },
         elasticOut: function (x, t, b, c, d) {
-            var s=1.70158;var p=0;var a=c;
-            if (t==0) return b;  if ((t/=d)==1) return b+c;  if (!p) p=d*.3;
-            if (a < Math.abs(c)) { a=c; var s=p/4; }
-            else var s = p/(2*Math.PI) * Math.asin (c/a);
-            return a*Math.pow(2,-10*t) * Math.sin( (t*d-s)*(2*Math.PI)/p ) + c + b;
+            var s = 1.70158; var p = 0; var a = c;
+            if (t == 0) return b; if ((t /= d) == 1) return b + c; if (!p) p = d * .3;
+            if (a < Math.abs(c)) { a = c; var s = p / 4; }
+            else var s = p / (2 * Math.PI) * Math.asin(c / a);
+            return a * Math.pow(2, -10 * t) * Math.sin((t * d - s) * (2 * Math.PI) / p) + c + b;
         }
     });
 
@@ -271,186 +275,10 @@
 
 })(jQuery);
 
-function blink(element){
+function blink(element) {
 
-    element.animate({ opacity: 0 }, 200, 'linear', function(){
+    element.animate({ opacity: 0 }, 200, 'linear', function () {
         $(this).animate({ opacity: 1 }, 200);
     });
 
 }
-
-particlesJS("particles-js", {
-    "particles": {
-        "number": {
-            "value": 355,
-            "density": {
-                "enable": true,
-                "value_area": 789.1476416322727
-            }
-        },
-        "color": {
-            "value": "#ffffff"
-        },
-        "shape": {
-            "type": "circle",
-            "stroke": {
-                "width": 0,
-                "color": "#000000"
-            },
-            "polygon": {
-                "nb_sides": 5
-            },
-            "image": {
-                "src": "img/github.svg",
-                "width": 100,
-                "height": 100
-            }
-        },
-        "opacity": {
-            "value": 0.48927153781200905,
-            "random": false,
-            "anim": {
-                "enable": true,
-                "speed": 0.2,
-                "opacity_min": 0,
-                "sync": false
-            }
-        },
-        "size": {
-            "value": 2,
-            "random": true,
-            "anim": {
-                "enable": true,
-                "speed": 2,
-                "size_min": 0,
-                "sync": false
-            }
-        },
-        "line_linked": {
-            "enable": false,
-            "distance": 150,
-            "color": "#ffffff",
-            "opacity": 0.4,
-            "width": 1
-        },
-        "move": {
-            "enable": true,
-            "speed": 0.2,
-            "direction": "none",
-            "random": true,
-            "straight": false,
-            "out_mode": "out",
-            "bounce": false,
-            "attract": {
-                "enable": false,
-                "rotateX": 600,
-                "rotateY": 1200
-            }
-        }
-    },
-    "interactivity": {
-        "detect_on": "canvas",
-        "events": {
-            "onhover": {
-                "enable": true,
-                "mode": "bubble"
-            },
-            "onclick": {
-                "enable": true,
-                "mode": "push"
-            },
-            "resize": true
-        },
-        "modes": {
-            "grab": {
-                "distance": 400,
-                "line_linked": {
-                    "opacity": 1
-                }
-            },
-            "bubble": {
-                "distance": 83.91608391608392,
-                "size": 1,
-                "duration": 3,
-                "opacity": 1,
-                "speed": 3
-            },
-            "repulse": {
-                "distance": 200,
-                "duration": 0.4
-            },
-            "push": {
-                "particles_nb": 4
-            },
-            "remove": {
-                "particles_nb": 2
-            }
-        }
-    },
-    "retina_detect": true
-});
-
-// const renderSlot = () => { 
-// let post = document.createElement('div')
-// post.innerHTML = `
-//                 <div class="modal-content">
-//                   <div class="modal-header">
-//                     <h5 class="modal-title">${bet.name}</h5>
-//                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-//                   </div>
-//                   <div class="modal-body">
-//                     <h6>Current Betting Values</h6>
-//                     <p class="card-text">For: ${bet.for_value} <i class="bi bi-emoji-smile-upside-down"></i>; Against: ${bet.against_value} <i class="bi bi-emoji-smile-upside-down"></i></p>
-//                     <p>${bet.description}</p>
-//                     <br>
-//                     <h6>Witnesses:</h6>
-//                     <p id="witnesslist"></p>
-//                     <br>
-//                     <h6>Participants:</h6>
-//                     <p id="counter"></p>
-//                     <p id="participantlist"></p>
-//                     <br>
-//                     <h7 class="card-subtitle mb-2 text-muted">Created by ${creator.data.username} at ${bet.createdAt}</h7>
-//                   </div>
-//                   <hr>
-//                   <div class="text-center" id="modalfooter">
-//                     <form>
-//                       <input class="form-control form-control-sm" type="text" id='aligntrue' placeholder="Whole number of Tokens" aria-label=".form-control-sm example"><i class="bi bi-emoji-smile-upside-down"></i>
-//                       <button type="button" class="joinbtn btn btn-primary" data-betid="${bet.id}" data-align="true" data-value="${bet.for_value}">Bet With</button>
-//                     </form>
-//                     <form>
-//                       <input class="form-control form-control-sm" type="text" id='alignfalse' placeholder="Whole number of Tokens" aria-label=".form-control-sm example">
-//                       <button type="button" class="joinbtn btn btn-primary" data-betid="${bet.id}" data-align="false" data-value="${bet.against_value}">Bet Against</button>
-//                     </form>
-//                   </div>
-//                 </div>
-//                 `
-// document.getElementById('modalContent').append(post)
-// }
-// renderSlot()
-// const renderSlot = () => {
-// let post = document.createElement('div')
-// post.className = "col-sm-6 col-md-4 col-xl-3"
-// post.innerHTML = `
-//                   <div class="card border-dark mb-3">
-//                     <div class="card-body">
-//                       <h5 class="card-title">${bet.name}</h5>
-//                       <h7>Current Betting Values</h7>
-//                       <p class="card-text">For: ${bet.for_value} <i class="bi bi-emoji-smile-upside-down"></i>; Against: ${bet.against_value} <i class="bi bi-emoji-smile-upside-down"></i></p>
-//                       <p class="card-text">${bet.description}</p>
-//                     </div>
-//                     <div class="card-footer">
-//                       <a data-bs-toggle="modal" data-betid="${bet.id}" href="#betModal" class="modalbtn btn btn-primary" style="width: 100%">View this Bet</a>
-//                     </div>
-//                   </div>
-//               `
-//     document.getElementById('content').append(post)
-// }
-// renderSlot()
-
-// document.addEventListener('click', event => {
-//     if (event.target.classList.contains('modalbtn')) {
-//         let slot = event.target.getAttribute('data-betid')
-//         renderSlot()
-//     }
-// })
--- routes/homeRoutes.js
+++ routes/homeRoutes.js
@@ -4,43 +4,47 @@ const passport = require('passport')
 const sequelize = require('../db')
 
 // login
-router.get('/login', (req, res) => {
-  res.render('login')
+router.get('/login', (req, res, next) => {
+  res.render('login', { css: ['style.css'], js: ['login.js']})
 })
 
 // register
-router.get('/register', (req, res) => {
-  res.render('register')
+router.get('/register', (req, res, next) => {
+  res.render('register', {css: ['register.css']})
 })
 
 // // open post
-// router.get('/blog/:id', (req, res) => {
+// router.get('/blog/:id', (req, res, next) => {
 //   res.render('blog', { blogid: req.params.id })
 // })
 
 // // edit post
-// router.get('/update/:id', (req, res) => {
+// router.get('/update/:id', (req, res, next) => {
 //   res.render('update', { editid: req.params.id })
 // })
 
 // dashboard
-router.get('/dashboard', (req, res) => {
-  res.render('dashboard')
+router.get('/dashboard', (req, res, next) => {
+  res.render('dashboard', {css: ['style.css']})
 })
 
 // new post
-router.get('/newbets', (req, res) => {
-  res.render('newbets')
+router.get('/newbets', (req, res, next) => {
+  res.render('newbets', {css: ['style.css'], js: ['login.css']})
 })
 
 // default get coins page
-router.get('/coins', (req, res) => {
-  res.render('coins')
+router.get('/coins', (req, res, next) => {
+  res.render('coins', { css: ['coins.css'], js: ['coins.js']})
+})
+
+router.get('/profile', (req, res, next) => {
+  res.render('profile', { css: ['profile.css'], js: ['profile.js']})
 })
 
 // default home
-router.get('/*', (req, res) => {
-  res.render('home')
+router.get('/*', (req, res, next) => {
+  res.render('home', {css: ['.css']})
 })
 
 
